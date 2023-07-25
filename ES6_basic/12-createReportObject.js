2cfe66c9aa789c75708f3ad6ad41b2441c13ecb2
annlaure02 <alaure.g02@gmail.com>
2023-07-25 13:03:47 +0200

    add semicolon line 5

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,5 +2,5 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
-  }
+  };
 }
