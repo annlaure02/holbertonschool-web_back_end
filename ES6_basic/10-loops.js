dd4eb57a6b206c89133490caaf365168cf2031d4
annlaure02 <alaure.g02@gmail.com>
2023-07-25 11:33:07 +0200

    changed let to const in the loop, eslint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
 
-  for (let element of array) {
+  for (const element of array) {
     array2.push(appendString + element);
   }
 
