824c1a51cd6ecd2ce15e9ef6cc02f9161fe5ffa3
annlaure02 <alaure.g02@gmail.com>
2023-07-26 10:37:35 +0200

    added file for task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
-  return Promise.allSettled(promises)
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises);
 }
