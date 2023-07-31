6f55908ce2b6b760a7b5e57d59c66215c91bb3a4
annlaure02 <alaure.g02@gmail.com>
2023-07-31 22:28:12 +0200

    added file for task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  let newString = '';
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      newString += `${element.replace(startString, '')}-`;
+    }
+  }
+  newString = newString.slice(0, -1);
+  return newString;
+}
