928f3457a461dcfb5155cdb788de2b644f507349
annlaure02 <alaure.g02@gmail.com>
2023-07-31 13:23:56 +0200

    fix eslint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
 export default function getListStudentIds(arrayOfObj) {
   if (typeof arrayOfObj !== 'object') {
     return [];
-  } else if (arrayOfObj === null) {
+  }
+  if (arrayOfObj === null) {
     return [];
   }
   return arrayOfObj.map((element) => element.id);
