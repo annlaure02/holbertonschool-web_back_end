ba2255383ef1e8e351a5463727b61b7c1bd5a737
annlaure02 <alaure.g02@gmail.com>
2023-07-31 13:18:48 +0200

    added file for task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
 export default function getListStudentIds(arrayOfObj) {
   if (typeof arrayOfObj !== 'object') {
     return [];
+  } else if (arrayOfObj === null) {
+    return [];
   }
   return arrayOfObj.map((element) => element.id);
 }
